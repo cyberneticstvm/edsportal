c6419ded476f3e149f1fa1f542609d90cc40c662
Cybernetics <mail@cybernetics.me>
2026-02-24 22:18:38 +0530

    small updates

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -4,11 +4,11 @@
     <div class="mb-4 pt-3">
         <div class="row">
             <div class="col-12">
-                <h5 class="fw-medium text-uppercase mb-0">Update Student</h5>
-                <p class="fs-12">Update Student</p>
+                <h5 class="fw-medium text-uppercase mb-0">Create Student</h5>
+                <p class="fs-12">Create Student</p>
             </div>
         </div>
-        {{ html()->form('POST')->route('student.create')->class('')->open() }}
+        {{ html()->form('POST')->route('student.save')->class('')->open() }}
         <div class="row g-lg-12 g-3">
             <div class="control-group col-md-3">
                 <label class="form-label req">Student Name </label>
